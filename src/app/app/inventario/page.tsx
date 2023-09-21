4752c03f7cc48ec9a55768960923f465891973aa
Facundo Desktop <facundev@outlook.com>
2023-09-21 16:37:03 -0300

    InventarioPage modified

--- src/app/app/inventario/page.tsx
+++ src/app/app/inventario/page.tsx
@@ -1,28 +1,16 @@
 'use client'
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 import { useEffect } from "react";
 
 function InventarioPage () {
 
     useEffect(()=>{
         fetch('https://ventify-back.azurewebsites.net/api/productos')
-<<<<<<< Updated upstream
-        .then(res => res.json())
-        .then(res => console.log(res))
-    })
-
-    return <h1>Inventario</h1>
-=======
         .then((res) => console.log(res.json()))
     },[])
 
     return(
         <h1>Inventario!</h1>
     )
->>>>>>> Stashed changes
 }
 
 export default InventarioPage;
