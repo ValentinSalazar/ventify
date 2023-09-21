017abbe3664b3299031fb7ba4b72832e80124de2
Facundo Desktop <facundev@outlook.com>
2023-09-21 16:17:23 -0300

    Checking main

--- src/app/app/inventario/page.tsx
+++ src/app/app/inventario/page.tsx
@@ -1,4 +1,15 @@
+'use client'
+
+import { useEffect } from "react";
+
 function InventarioPage () {
+
+    useEffect(()=>{
+        fetch('https://ventify-back.azurewebsites.net/api/productos')
+        .then(res => res.json())
+        .then(res => console.log(res))
+    })
+
     return <h1>Inventario</h1>
 }
 
